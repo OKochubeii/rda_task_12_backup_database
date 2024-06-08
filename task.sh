84e54e464e01a9b2630cd143dda6370a82b79c3d
OKochubeii <155968394+OKochubeii@users.noreply.github.com>
2024-06-08 22:30:28 +0300

    Resolved

--- task.sh
+++ task.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=full_backup.sql
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < full_backup.sql
-
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --no-create-info --result-file=data_backup.sql
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data_backup.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-db --result-file=full-backup.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < full-backup.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB --no-create-info --result-file=data-backup.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data-backup.sql
